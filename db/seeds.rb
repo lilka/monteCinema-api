9476e1564495773db6f72267e729d008cc41d7e1
lilka <lili.lomnicka@gmail.com>
2021-08-06 11:16:13 +0200

    models added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+movies = Movie.create([{title: 'Star Wars', description: 'movie info #1', duration_min: 123}, {title: 'Lord of the Rings', description: 'movie info #2', duration_min: 143}])
