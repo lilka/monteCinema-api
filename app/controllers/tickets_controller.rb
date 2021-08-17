7fa26fa9106e4a50a3552e914346b9c1bc6263ec
lilka <lili.lomnicka@gmail.com>
2021-08-18 00:55:03 +0200

    delete ticket controller

--- app/controllers/tickets_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class TicketsController < ApplicationController
-  def generateTickets(_reservation_id, seats_amount)
-    seats_amount.all do |_seat|
-      ticket = Ticket.create!({ reservation_id: :reservation_id })
-    end
-  end
-end
