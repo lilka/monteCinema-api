2304747e3740a63c3fb0a254ef340f1fcf9a8ebd
lilka <lili.lomnicka@gmail.com>
2021-08-17 09:52:51 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# movies = Movie.create([{ title: 'Star Wars', description: 'movie info #1', duration_min: 123 },
-#                      { title: 'Lord of the Rings', description: 'movie info #2', duration_min: 143 }])
+require 'faker'
 
-# cinema_halls = CinemaHall.create([{ number_of_seats: 100, name: 'Krolewna sniezka' },
-#                                 {  number_of_seats: 200, name: 'Puchatek'}])
 
-screenings = Screening.create([{ cinema_hall_id: 8, movie_id: 1, start_time: 17.00, date: Time.zone.now.to_s(:db) }])
+5.times do 
+  Movie.create(title: Faker::Movie.title, duration: rand(75...300), description: Faker::Movie.quote)
+end
+
+CinemaHall.create(name: Faker::Fantasy::Tolkien.character, number_of_seats: 200)
+CinemaHall.create(name: Faker::Fantasy::Tolkien.character, number_of_seats: 100)
+CinemaHall.create(name: Faker::Fantasy::Tolkien.character, number_of_seats: 20)
+
+9.times do 
+    Screening.create(cinema_hall_id: CinemaHall.ids.sample, movie_id: Movie.ids.sample, start_time: 17, date: Faker::Date.in_date_period)
+end
