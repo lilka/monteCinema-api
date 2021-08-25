b47edbc32b6885830e64c7a56878ba649672dfb8
lilka <lili.lomnicka@gmail.com>
2021-08-25 15:53:48 +0200

    schema update

--- db/migrate/20210812132449_change_cinemahall_fields.rb
+++
@@ -1,7 +0,0 @@
-class ChangeCinemahallFields < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :cinema_halls :row_number 
-    remove_column :cinema_halls :seats_in_row
-    add_column :cinema_halls :number_of_seats 
-  end
-end
