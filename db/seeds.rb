cbd3e47f5d040933393ff05a01b91e92fb7de04c
Lila Łomnicka <lili.lomnicka@gmail.com>
2021-08-26 22:42:51 +0200

    Update db/seeds.rb
    Co-authored-by: Wojciech Stępniak <wojciech.stepniak@monterail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,11 +17,11 @@ CinemaHall.create(name: Faker::Fantasy::Tolkien.character, number_of_seats: 20)
 end
 
 
-Role.create(role_name: 'client')
-Role.create(role_name: 'employee')
+role_1 = Role.create(role_name: 'client')
+role_2 = Role.create(role_name: 'employee')
 Role.create(role_name: 'manager')
 
-User.create(email: "user1@test.com", password: "password1", password_confirmation: "password1", role_id: 1 )
-User.create(email: "employee@test.com", password: "password2", password_confirmation: "password2", role_id: 2 )
+User.create(email: "user1@test.com", password: "password1", password_confirmation: "password1", role: role_1 )
+User.create(email: "employee@test.com", password: "password2", password_confirmation: "password2", role: role_2 )
 
 
