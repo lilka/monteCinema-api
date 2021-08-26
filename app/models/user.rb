1b4d64fd4d9cdcd82d754ccb25f1ceb0977898aa
lilka <lili.lomnicka@gmail.com>
2021-08-26 12:35:26 +0200

    working on auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  before_action :authenticate_user!
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
