5370eb80687de6054d03fbd3df45a36a1980b8e2
lilka <lili.lomnicka@gmail.com>
2021-09-02 15:25:48 +0200

    add reservation cancellation email

---
+++ app/mailers/reservation_cancellation_mailer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class ReservationConfirmationMailer < ApplicationMailer
+  def cancellation_email
+    @reservation = params[:reservation]
+    @seats_reservation = ReservationSeats::Repository.new.fetch_seats_for_reservation(@reservation.id)
+    @user =  @reservation.user
+    mail(to: @user.email, subject: 'MonteCinema - reservation cancellation')
+  end
+
+  private
+
+  def find_user(user_id)
+    Users::Repository.new.find(user_id)
+  end
+end
