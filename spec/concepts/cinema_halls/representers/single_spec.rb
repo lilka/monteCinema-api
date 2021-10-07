23955ac673df3b4e2f8b1237cde99b1ea83d42d7
Lila Lomnicka <lilalomnicka@MBP-Lila.ad.monterail.com>
2021-10-07 09:12:50 +0200

    add cinema hall reperesenter test

---
+++ spec/concepts/cinema_halls/representers/single_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CinemaHalls::Representers::Single do 
+    it "represents single object" do 
+        cinema_hall = create(:cinema_hall)
+        expect(described_class.new(cinema_hall).call).to eq(
+            :id => cinema_hall.id, 
+            :name => cinema_hall.name, 
+            :number_of_seats => cinema_hall.number_of_seats
+        )
+    end
+end 
